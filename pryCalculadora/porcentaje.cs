d56c35e0ed48d77d4595c3208cf6506bae2f62fa
Jesus Alexis <20251037@uthh.edu.mx.>
2026-06-04 14:27:54 -0600

    porcentaje

---
+++ pryCalculadora/porcentaje.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryCalculadora
+{
+    internal class porcentaje
+    {
+        public float Numero1;
+        public float Numero2;
+    }
+}
