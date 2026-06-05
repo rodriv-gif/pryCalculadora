eb8febc921f0686295cd9bf62e9c0d804c4e2b24
uriele-boop <20250997@uthh.edu.mx>
2026-06-04 22:08:50 -0600

    Creacion de clase potencia

---
+++ pryCalculadora/clsPotencia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryCalculadora
+{
+    internal class clsPotencia
+    {
+        public float resultado;
+
+        public void Potencia(float numero1, float numero2)
+        {
+            resultado = (float)Math.Pow(numero1, numero2);
+        }
+    }
+}
