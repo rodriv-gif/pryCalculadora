94f091a44e8e707f3a1e07da8c3daa3e263814b7
BRYAN BENITEZ <20251077@uthh.edu.mx>
2026-06-08 08:17:48 -0600

    bryan wymer se creo estas variables para realizar un calculo que aun no esta programado el proceso

---
+++ pryCalculadora/Clsbryan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryCalculadora
+{
+    internal class Clsbryan
+    {
+        public float numero1;
+        public float numero2;
+
+        
+        public float resultado;
+
+    }
+}
