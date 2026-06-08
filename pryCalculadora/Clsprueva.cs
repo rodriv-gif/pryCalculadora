60aed7e0cfba14aed39c18b24512ad5ad383165d
BRYAN BENITEZ <20251077@uthh.edu.mx>
2026-06-07 20:11:57 -0600

    si

---
+++ pryCalculadora/Clsprueva.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryCalculadora
+{
+    internal class Clsprueva
+    {
+        public float numero1;
+        public float numero2;
+
+        public float resultado;
+    }
+}
